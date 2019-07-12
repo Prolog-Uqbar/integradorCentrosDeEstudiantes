0d458c4e04b0ba8413679f5b3d35bbf3a0f200b6
Lucas Spigariol <lspigariol@gmail.com>
2019-07-12 14:27:24 -0300

    Update centros.pl

--- centros.pl
+++ centros.pl
@@ -1,19 +1,23 @@
-
+%elecciones(año).
 elecciones(2017).
 elecciones(2019).
 
-estudiantes(sistemas, 2019, juanPerez).
-estudiantes(sistemas, 2019, unter).
-estudiantes(sistemas, 2019, mathy).
-estudiantes(sistemas, 2017, unter).
-estudiantes(sistemas, 2017, mathy).
-estudiantes(sistemas, 2018, unter).
+%estudiante(departamento, año, nombre).
+estudiante(sistemas, 2019, juanPerez).
+estudiante(sistemas, 2019, unter).
+estudiante(sistemas, 2019, mathy).
+estudiante(sistemas, 2017, unter).
+estudiante(sistemas, 2017, mathy).
+estudiante(sistemas, 2018, unter).
+estudiante(quimica, 2018, cacho).
 
-votos(franjaNaranja, 2500, sistemas, 2017).
-votos(franjaNaranja, 2152, sistemas, 2019).
-votos(agosto29, 710, sistemas, 2019).
-votos(seu, 917, sistemas, 2019).
+%votos(agrupacion, votos, año).
+votos(franjaNaranja, 2500, 2017).
+votos(franjaNaranja, 2152, 2019).
+votos(agosto29, 710, 2019).
+votos(seu, 917, 2019).
 
+%realizoAccion(agrupacion, accion).
 realizoAccion(franjaNaranja, lucha(salarioDocente)).
 realizoAccion(franjaNaranja, gestionIndividual(“Excepción de correlativas”, juanPerez, 2019)).
 realizoAccion(franjaNaranja, obra(2019)).
