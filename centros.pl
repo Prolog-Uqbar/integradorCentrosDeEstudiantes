7d653eb02ff2b475ef6e1fbc532c6c40b76ba48c
Lucas Spigariol <lspigariol@gmail.com>
2019-07-12 14:16:58 -0300

    Create centros.pl

---
+++ centros.pl
@@ -0,0 +1,21 @@
+
+elecciones(2017).
+elecciones(2019).
+
+estudiantes(sistemas, 2019, juanPerez).
+estudiantes(sistemas, 2019, unter).
+estudiantes(sistemas, 2019, mathy).
+estudiantes(sistemas, 2017, unter).
+estudiantes(sistemas, 2017, mathy).
+estudiantes(sistemas, 2018, unter).
+
+votos(franjaNaranja, 2500, sistemas, 2017).
+votos(franjaNaranja, 2152, sistemas, 2019).
+votos(agosto29, 710, sistemas, 2019).
+votos(seu, 917, sistemas, 2019).
+
+realizoAccion(franjaNaranja, lucha(salarioDocente)).
+realizoAccion(franjaNaranja, gestionIndividual(“Excepción de correlativas”, juanPerez, 2019)).
+realizoAccion(franjaNaranja, obra(2019)).
+realizoAccion(agosto29, lucha(salarioDocente)).
+realizoAccion(agosto29, lucha(boletoEstudiantil)).
